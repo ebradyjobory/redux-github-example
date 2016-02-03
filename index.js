3e762d36802b807a37732486186802b6a637c3eb
essamjoubori <essam.joubori@gmail.com>
2016-02-03 10:54:06 -0500

    Add sacss compiler

---
+++ index.js
@@ -0,0 +1,13 @@
+require('./stylesheets/style.scss');
+import 'babel-core/polyfill'
+import React from 'react'
+import { render } from 'react-dom'
+import Root from './containers/Root'
+import configureStore from './store/configureStore'
+
+const store = configureStore();
+
+render(
+  <Root store={store} />,
+  document.getElementById('root')
+);
